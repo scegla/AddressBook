1dd7eed4f9d1441dcc9031ea8a8954fe914fec2a
scegla <60694330+scegla@users.noreply.github.com>
2020-02-05 12:13:57 +0100

    Add files via upload

---
+++ Scripts/app.js
@@ -0,0 +1,40 @@
+﻿var ViewModel = function () {
+    var self = this;
+    self.contacts = ko.observableArray();
+    self.error = ko.observable();
+
+    var contactsUri = '/api/contacts/';
+
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            self.error(errorThrown);
+        });
+    }
+
+    function getAllContacts() {
+        ajaxHelper(contactsUri, 'GET').done(function (data) {
+            self.contacts(data);
+        });
+
+    }
+
+    // Fetch the initial data.
+    getAllContacts();
+
+    self.detail = ko.observable();
+
+    self.getContactDetail = function (item) {
+        ajaxHelper(contactsUri + item.Id, 'GET').done(function (data) {
+            self.detail(data);
+        });
+    }
+};
+
+ko.applyBindings(new ViewModel());
