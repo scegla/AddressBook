54d42440092148e85e1167d59fa22e079c989485
scegla <60694330+scegla@users.noreply.github.com>
2020-02-13 10:44:00 +0100

    Add files via upload

---
+++ Migrations/202002100906318_migrate.cs
@@ -0,0 +1,16 @@
+namespace PhoneBooth.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migrate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
